ac64af7f9b99f6243c19a2f4b2d32b723f74a4f0
swygax <kennykysv@gmail.com>
2022-08-06 07:20:13 -0300

    Corrected bug in resume menu: inverted download links

--- pages/resume/ResumeMenu.js
+++ pages/resume/ResumeMenu.js
@@ -2,17 +2,17 @@ import Box from '../../components/Box'
 import Viewer from './PdfViewer'
 
 export default function ResumeMenu() {
-  var links = [ "/files/KennyVargas-en_US.pdf", "/files/KennyVargas-pt_BR.pdf" ]
+  var links = ["/files/KennyVargas-pt_BR.pdf", "/files/KennyVargas-en_US.pdf"]
 
   return (
     <div>
       <label>Português: </label><Box type='external' style='styled' link={links[0]}>Baixar</Box>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <label>English: </label><Box type='external' style='styled' link={links[1]}>Download</Box>
-      <br/>
-      <br/>
-      <div className='frame'><Viewer source={links[1]} /></div>
+      <br />
+      <br />
+      <div className='frame'><Viewer source={links[0]} /></div>
     </div>
   )
 }
