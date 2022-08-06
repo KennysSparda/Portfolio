9a4a20c3d119bbfe24e70c8bbf35417bdde5ce56
swygax <kennykysv@gmail.com>
2022-08-06 09:17:00 -0300

    Added real informations to Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Copyright from "./copyright";
+
 /*    Footer like place for some info 
  *      you will need to load this on
  *      all pages manualy :(
@@ -6,8 +8,8 @@ export default function Footer() {
   return (
     <footer className="footer" id="unstyled">
       {/* You can make changes below */}
-      <p>Posted by Swygax</p>
-      <p>Localized at rua chabvlas, 25, Jobes Fragnoth / FR</p>
+      <p>06 Aug 2022 - Campo Largo / PR</p>
+      <Copyright />
     </footer>
   )
 }
