b3bcacffc92c8a3a29c21306fc12a31116e8f96f
kennyssparda <kennykysv@gmail.com>
2023-03-31 18:17:31 -0300

    Added new project: KySound

---
+++ public/projects/KySound/script.js
@@ -0,0 +1,64 @@
+function button1play() {
+  function play() {
+    let x = document.getElementById("audio1"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button2play() {
+  function play() {
+    let x = document.getElementById("audio2"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button3play() {
+  function play() {
+    let x = document.getElementById("audio3"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button4play() {
+  function play() {
+    let x = document.getElementById("audio4"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button5play() {
+  function play() {
+    let x = document.getElementById("audio5"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button6play() {
+  function play() {
+    let x = document.getElementById("audio6"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button7play() {
+  function play() {
+    let x = document.getElementById("audio7"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button8play() {
+  function play() {
+    let x = document.getElementById("audio8"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+function button9play() {
+  function play() {
+    let x = document.getElementById("audio9"); 
+    x.cloneNode(true).play(); 
+  }
+  play()
+}
+
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,6 +13,7 @@ import Projeto3 from '../components/projects/Projeto3'
 import Projeto4 from '../components/projects/Projeto4'
 import Projeto5 from '../components/projects/Projeto5'
 import Projeto6 from '../components/projects/Projeto6'
+import Projeto7 from '../components/projects/Projeto7'
 
 export default function Projects() {
   const [lightTheme, setLightTheme] = useState(false)
@@ -52,6 +53,7 @@ export default function Projects() {
           <Projeto4/>
           <Projeto5/>
           <Projeto6/>
+          <Projeto7/>
         </div>
         <Footer/>
       </div>
