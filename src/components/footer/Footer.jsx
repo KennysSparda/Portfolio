b70a592d65b4712fa556c7eb6426d16dfe1f2005
KennyVargas <kennykysv@gmail.com>
2023-06-07 04:25:24 -0300

    Media query integration in css and renamed to jsx extension Projects and ProjectModel

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer>
       {/* You can make changes below */}
-      <p id="footerText">07/06/2023 - Campo Largo / PR | version: 3.5.0</p>
+      <p id="footerText">07/06/2023 - Campo Largo / PR</p>
+      <p>versão do projeto: 3.5.1</p>
      <div className="copyright">KennyVargas ©</div> 
     </footer>
   )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 
 import Home from '../components/home/Home'
-import Projects from '../components/projects/Projetos'
+import Projects from '../components/projects/Projects'
 import About from '../components/about/About'
 
 export default function App() {
