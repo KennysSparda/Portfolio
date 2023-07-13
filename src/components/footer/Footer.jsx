907bdebf66fbb1e56143e0e0c5bb71d921bace5e
KennyVargas <kennykysv@gmail.com>
2023-07-12 21:02:45 -0300

    Added effect on img perfil

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer>
       {/* You can make changes below */}
-      <p id="footerText">11/07/2023 - Campo Largo / PR</p>
-      <p>versão do projeto: 3.7.1</p>
+      <p id="footerText">12/07/2023 - Campo Largo / PR</p>
+      <p>versão do projeto: 3.7.2</p>
      <div className="copyright">KennyVargas ©</div> 
     </footer>
   )
