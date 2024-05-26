6094d4fa8a37d44385e35ddcebdf523249ad7caa
KennyVargas <kennykysv@gmail.com>
2024-05-26 01:33:33 -0300

    Responsabilidade do rodape corrigida

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,14 +5,16 @@ import BtnWhatsapp from '../buttons/btnWhatsapp';
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex items-center justify-center flex-wrap lg:flex-col space-x-2 space-y-2 mb-8">
-        <BtnGithub className="mb-2 lg:mb-0 lg:mr-2" />
-        <BtnLinkedin className="mb-2 lg:mb-0 lg:mr-2" />
+      <div className="container mx-auto flex flex-col items-center space-y-4 md:space-y-0 md:flex-row md:justify-center md:space-x-4">
+        <BtnGithub />
+        <BtnLinkedin />
         <BtnWhatsapp />
       </div>
-      <p className="text-center text-gray-500 mb-2">Atualizado em 20/02/2024 | Campo Largo, PR</p>
-      <p className="text-center text-gray-500 mb-2">Versão do projeto: 4.0.0</p>
-      <div className="text-center text-gray-500">© Kenny Vargas 2024</div>
+      <div className="mt-8 text-center text-gray-500">
+        <p className="mb-2">Atualizado em 20/02/2024 | Campo Largo, PR</p>
+        <p className="mb-2">Versão do projeto: 4.0.0</p>
+        <p>© Kenny Vargas 2024</p>
+      </div>
     </footer>
   );
 }
